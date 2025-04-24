1b5468b711081ab8f36094b6126df4a85a3b6146
batasi <batasisolomon029@gmail.com>
2025-04-24 08:09:30 +0000

    Fix Attendancies model casing

--- app/Models/Attendancies.php
+++ app/Models/Attendancies.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class attendancies extends Model
+class Attendancies extends Model
 {
     protected $fillable = [
         'employee_id',
