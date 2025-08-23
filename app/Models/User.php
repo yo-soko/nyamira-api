dd23d15db0873a2957500070f3deb6bad2869d3f
batasi <batasisolomon029@gmail.com>
2025-08-23 18:50:28 +0300

    credentials_updated in student model

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,7 +19,7 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name', 'email', 'phone', 'role', 'code', 'profile_picture', 'password', 'status'
+        'name', 'email', 'phone', 'role', 'code', 'profile_picture', 'password', 'status','credentials_updated '
     ];
 
     /**
