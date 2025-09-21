8ed22dfd243ec4f73b255e32463f2f363496ff23
batasi <batasisolomon029@gmail.com>
2025-09-21 11:56:53 +0300

    fixed

--- resources/views/assignments/_form.blade.php
+++ resources/views/assignments/_form.blade.php
@@ -32,7 +32,7 @@
         </div>
     </div>
     <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
         <button type="submit" class="btn btn-primary">Save Assignment</button>
     </div>
 </form>
--- resources/views/assignments/create.blade.php
+++ resources/views/assignments/create.blade.php
@@ -79,7 +79,7 @@
     <div class="modal-content">
         <div class="modal-header">
             <h5 class="modal-title">Add Assignment</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">&times;</button>
+            <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
         </div>
         @include('assignments._form')
     </div>
